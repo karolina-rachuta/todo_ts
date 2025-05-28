13690cc4858c3c461771ac6a56606fafaab9935f
Karolina Rachuta <karolina.rachuta@gmail.com>
2025-05-28 20:46:18 +0200

    place to write the notes and adding them to display as a list below

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [note, setNote] = useState("")
+    const [notes, setNotes] = useState<string[]>([])
+
+    function handleAddingNote(e: any): any {
+        const newNote = note;
+        setNotes([...notes, newNote]);
+        setNote('')
+    }
+    return (
+        <div>
+            <div>
+                <label htmlFor="notes">Your notes</label>
+                <input type="text" placeholder='todo' name="notes" id="notes" value={note} onChange={(e) => setNote(e.target.value)} />
+                <button type="button" onClick={handleAddingNote}>Add</button>
+            </div>
+            <div>
+                <h2>List of todos:</h2>
+                <ul>{notes.map((note, index) => (<li key="">{note} <button type="button">-</button><button type='button'>tick</button></li>))}</ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
