581fd28df770cbaf93f85ec681751487d92f6054
Karolina Rachuta <karolina.rachuta@gmail.com>
2025-05-28 21:20:02 +0200

    adding option to delete tasks/notes

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,35 @@ import React, { useState } from 'react';
 
 
 function App() {
-    const [note, setNote] = useState("")
-    const [notes, setNotes] = useState<string[]>([])
+    const [note, setNote] = useState("");
+    const [notes, setNotes] = useState<string[]>([]);
 
     function handleAddingNote(e: any): any {
+        if (!note) return;
         const newNote = note;
         setNotes([...notes, newNote]);
         setNote('')
     }
+
+    function handleDelete(idToDelete: number) {
+        const updatedNotes = notes.filter((_, index) => index !== idToDelete);
+        setNotes(updatedNotes);
+    }
+
+    function handleDoneTasks(idDoneTask: number) {
+
+    }
     return (
         <div>
             <div>
+                <h1>TODO APP IN TS</h1>
                 <label htmlFor="notes">Your notes</label>
                 <input type="text" placeholder='todo' name="notes" id="notes" value={note} onChange={(e) => setNote(e.target.value)} />
                 <button type="button" onClick={handleAddingNote}>Add</button>
             </div>
             <div>
                 <h2>List of todos:</h2>
-                <ul>{notes.map((note, index) => (<li key="">{note} <button type="button">-</button><button type='button'>tick</button></li>))}</ul>
+                <ul>{notes.map((note, index) => (<li key={index}>{note} <button type="button" >-</button><button type='button' onClick={() => handleDoneTasks(index)}>tick</button></li>))}</ul>
             </div>
         </div>
     );
