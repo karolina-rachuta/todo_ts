b42edf6f92370fa4c864cfe6a636c796edc7476f
Karolina Rachuta <karolina.rachuta@gmail.com>
2025-06-28 11:57:00 +0200

    adding status for the tasks, changing the status

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,32 @@ import React, { useState } from 'react';
 import './App.css';
 import { v4 as uuidv4 } from 'uuid';
 
+
+//Theme
 type TaskStatus = 'new' | 'in progress' | 'done' //3 columns
 
 type Task = {
     id: string,
     value: string,
-    status: TaskStatus
+    status: TaskStatus,
+    selected: boolean
 }
 
 type SelectedTasks = Record<string, boolean>
-//helpers fincion to updated the status
+//helpers funcion to updated the status
 //ask chatgpt how it can be improve in a smart way as a senior react developer to upgrade this, optimize performance ect
 
 function App() {
     const [task, setTask] = useState<Task | null>(null);
     const [existingTasks, setExistingTasks] = useState<Task[]>([]);
-    const [doneTasks, setDoneTasks] = useState<Task[]>([]);
-    const [hide, setHide] = useState(false);
     const [selectedTasks, setSelectedTasks] = useState<SelectedTasks>({});
+
 
 
     function handleAddingNote(): void {
         if (!task) return;
         setExistingTasks([...existingTasks, task]);
-        setTask(null); //fixed clearing the input
+        setTask(null);
 
     }
 
@@ -33,14 +35,20 @@ function App() {
         setSelectedTasks(prev => ({ ...prev, [idToSelect]: !prev[idToSelect] }))
     }
 
+    function handleChangeTaskStatus(e: React.ChangeEvent<HTMLSelectElement>, taskId: string): void {
+        const newStatus = e.target.value as TaskStatus;
+        setExistingTasks(prev => prev.map((
+            task => task.id === taskId ? { ...task, status: newStatus } : task)))
+    }
+
+
     function handleDelete(idToDelete: string): void {
-        // setExistingTasks(existingTasks.filter((task) => task.id !== idToDelete)); // update 
+        // setExistingTasks(existingTasks.filter((task) => task.id !== idToDelete)); // update
         setExistingTasks(prev => prev.filter((task) => task.id !== idToDelete)); //what is the difference // based on what was previously there
     }
 
-    function handleDoneTasks(idDoneTask: string, value: string): void {
-        setDoneTasks([...doneTasks, { id: idDoneTask, value: value, status: 'done' }]);
-        setExistingTasks(existingTasks.filter((task) => task.id !== idDoneTask));
+    function handleSelectAll() {
+
     }
 
     return (
@@ -55,41 +63,71 @@ function App() {
                     id="tasks"
                     // value={task?.value ?? ''}
                     value={task ? task.value : ''}
-                    onChange={(e) => setTask({ ...task, value: e.target.value, id: uuidv4(), status: 'done' })}
+                    onChange={(e) => setTask({ ...task, value: e.target.value, id: uuidv4(), status: 'new', selected: false })}
                 />
                 <button type="button" onClick={handleAddingNote}>Add</button>
             </div>
 
             <div className="todos-section">
-                <h2>List of todos:</h2>
-                <ul>
-                    {existingTasks.map((task) => (
-                        <li key={task.id} className={selectedTasks[task.id] ? "todo-item selected" : "todo-item"}>
-                            <span>{task.value}</span>
-                            <div className="actions">
-                                <button type="button" onClick={() => handleSelect(task.id)}>selected</button>
-                                <button type="button" onClick={() => handleDelete(task.id)}>delete</button>
-                                <button type='button' onClick={() => handleDoneTasks(task.id, task.value)}>done</button>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+                <div className="task-column">
+                    <h2>List of new todos:</h2>
+                    <ul>
+                        {existingTasks.filter((task) => task.status === 'new').map((task) => (
+                            <li key={task.id} className={selectedTasks[task.id] ? "todo-item selected" : "todo-item"}>
+                                <span>{task.value}</span>
+                                <div className="actions">
+                                    <button type="button" onClick={() => handleSelect(task.id)}>select</button>
+                                    <select className="custom-select" name="selectStatus" defaultValue="new" value={task.status} onChange={(e) => handleChangeTaskStatus(e, task.id)}>
+                                        <option value="new">New</option>
+                                        <option value="in progress">In progress</option>
+                                        <option value="done">Done</option>
+                                    </select>
+                                    <button type="button" onClick={() => handleDelete(task.id)}>delete</button>
 
-            <div className="done-section">
-                <button onClick={() => setHide(prev => !prev)}>
-                    {hide ? 'Show Done Tasks' : 'Hide Done Tasks'}
-                </button>
-                {!hide && doneTasks.length > 0 && (
-                    <div>
-                        <h2>Done tasks:</h2>
-                        <ul>
-                            {doneTasks.map(({ id, value }) => (
-                                <li key={id} className="done-item">{value}</li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="task-column">
+                    <h2>List of in progress todos:</h2>
+                    <ul>
+                        {existingTasks.filter((task) => task.status === 'in progress').map(inProgressTask => (
+                            <li key={inProgressTask.id} className={selectedTasks[inProgressTask.id] ? "todo-item selected" : "todo-item"}>
+                                <span>{inProgressTask.value}</span>
+                                <div className="actions">
+                                    <button type="button" onClick={() => handleSelect(inProgressTask.id)}>select</button>
+                                    <select className="custom-select" name="selectStatus" defaultValue="new" value={inProgressTask.status} onChange={(e) => handleChangeTaskStatus(e, inProgressTask.id)}>
+                                        <option value="new">New</option>
+                                        <option value="in progress">In progress</option>
+                                        <option value="done">Done</option>
+                                    </select>
+                                    <button type="button" onClick={() => handleDelete(inProgressTask.id)}>delete</button>
+
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="task-column">
+                    <h2>List of done:</h2>
+                    <ul>
+                        {existingTasks.filter((task) => task.status === 'done').map(doneTask => (
+                            <li key={doneTask.id} className={selectedTasks[doneTask.id] ? "todo-item selected" : "todo-item"}>
+                                <span>{doneTask.value}</span>
+                                <div className="actions">
+                                    <button type="button" onClick={() => handleSelect(doneTask.id)}>select</button>
+                                    <select className="custom-select" name="selectStatus" defaultValue="new" value={doneTask.status} onChange={(e) => handleChangeTaskStatus(e, doneTask.id)}>
+                                        <option value="new">New</option>
+                                        <option value="in progress">In progress</option>
+                                        <option value="done">Done</option>
+                                    </select>
+                                    <button type="button" onClick={() => handleDelete(doneTask.id)}>delete</button>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     );
